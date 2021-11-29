1ae74458be2bbf512f1758758de6dfd13e6d0f5d
Pranav Viswanathan <72350161+PranavViswanathan@users.noreply.github.com>
2021-11-29 20:47:32 +0530

    Update Parent.ino

--- Adafruit/Arduino/Parent.ino
+++ Adafruit/Arduino/Parent.ino
@@ -1,7 +1,9 @@
 // file name on system : hcsr.ino
+
 #include <SoftwareSerial.h>
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define buzzer 8
 SoftwareSerial espSerial(5, 6);
 // defines variables
 long duration; // variable for the duration of sound wave travel
@@ -11,6 +13,7 @@ String str;
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+    pinMode(buzzer, OUTPUT);
   Serial.begin(115200); // // Serial Communication is starting with 9600 of baudrate speed
   espSerial.begin(115200);
 }
@@ -31,7 +34,11 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-  
+  if(distance < 10) {
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+  }
+  digitalWrite(buzzer, LOW);
   espSerial.println(distance);
   delay(5000);
 }
